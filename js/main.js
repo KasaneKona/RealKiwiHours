eafe703c09f4a3656f5e18c507eb29a288cc3699
KasaneKona <kasanekona@gmail.com>
2020-01-19 14:30:09 +0000

    Add basic audio loop

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var renderer;
+var audioLoop;
 
 function main() {
 	var canvas = document.querySelector("#glCanvas");
@@ -11,7 +12,7 @@ function main() {
 	window.addEventListener('resize', resize, false);
 	resize();
 	renderer.initScene();
-	requestAnimationFrame(render);
+	audioLoop = createAudioLoop("/static/audio/loop.wav", 627200/44100);
 }
 
 function resize() {
@@ -27,11 +28,46 @@ function render(now) {
 	then = now;
 	
 	if(renderer.renderFrame(deltaTime)) {
-		this.animateRequest = requestAnimationFrame(render);
+		requestAnimationFrame(render);
 	} else main(); // Attempt to reload
 }
 
 // Run
 window.onload = () => {
 	main();
+}
+
+function createAudioLoop(url, len) {
+	audioToLoad++;
+	//console.log("Now waiting on "+audioToLoad+" audio files");
+	const aud = new Audio('/static/audio/loop.wav');
+	var loopObj = {aud:aud, len:len};
+	loopObj.readyFunc = () => {
+		audioLoaded();
+		aud.removeEventListener('canplaythrough', loopObj.readyFunc, false);
+	};
+	readyListener = aud.addEventListener('canplaythrough', loopObj.readyFunc, false);
+	return loopObj;
+}
+
+var audioToLoad = 0;
+function audioLoaded() {
+	audioToLoad--;
+	//console.log(audioToLoad+" audio files left to load");
+	if(audioToLoad==0) audioReady();
+}
+
+function audioReady() {
+	console.log("Audio ready");
+	requestAnimationFrame(render);
+	audioLoop.aud.addEventListener('play', ()=>audioLoopReset(audioLoop), false);
+	//audioLoopReset(audioLoop);
+	audioLoop.aud.play();
+}
+
+function audioLoopReset(loop) {
+	//console.log("Resetting audio");
+	if(loop.intv) clearInterval(loop.intv);
+	loop.aud.currentTime = 0;
+	loop.intv = setInterval(()=>{loop.aud.currentTime -= loop.len}, loop.len*1000);
 }
