50cd2f2e037e45738079e6d9879458d244b79b12
KasaneKona <kasanekona@gmail.com>
2020-07-05 01:52:47 +0100

    Replaced bad audio code with howler.js (Web Audio API)

--- js/main.js
+++ js/main.js
@@ -13,22 +13,39 @@ function main() {
 	}
 	window.addEventListener('resize', resize, false);
 	resize();
+	audioLoop = new Howl({
+		src: ["/static/audio/loop.wav"],
+		loop: true,
+		volume: listenVolume,
+		preload: true,
+		onplayerror: function() {
+			showAudioWarn();
+			audioLoop.once('unlock', hideAudioWarn);
+		}
+	});
 	renderer.initScene();
-	audioLoop = createAudioLoop("/static/audio/loop.wav", 627200/44100);
+	audioLoop.play();
+	// Hack: onplayerror doesn't work for web audio yet, trigger it manually if audio didn't start after 500ms
+	setTimeout(() => {
+		if(!audioLoop.playing()) {
+			if(!document.getElementById("audioWarnMsg")) {
+				audioLoop._onplayerror[0].fn();
+			}
+		}
+	}, 500);
+	render(0);
 }
 
 function resize() {
 	renderer.resize(window.innerWidth, window.innerHeight);
 }
 
-var then = 0;
-
 // Draw the scene repeatedly
+var then = 0;
 function render(now) {
 	now *= 0.001;  // convert to seconds
 	const deltaTime = now - then;
 	then = now;
-	
 	if(renderer.renderFrame(deltaTime)) {
 		requestAnimationFrame(render);
 	} else main(); // Attempt to reload
@@ -37,64 +54,26 @@ function render(now) {
 // Run
 window.onload = () => {
 	main();
-	document.onclick = ()=>{
-		var audioWarnMsg = document.getElementById("audioWarnMsg");
-		if(audioWarnMsg) audioWarnMsg.parentElement.removeChild(audioWarnMsg);
-		var aud = audioLoop.aud;
-		if(aud.paused || !aud.played) aud.play(); else aud.pause();
-	};
 }
 
-function createAudioLoop(url, len) {
-	audioToLoad++;
-	//console.log("Now waiting on "+audioToLoad+" audio files");
-	const aud = new Audio('./static/audio/loop.wav');
-	aud.volume = listenVolume;
-	var loopObj = {aud:aud, len:len};
-	loopObj.readyFunc = () => {
-		audioLoaded();
-		aud.removeEventListener('canplay', loopObj.readyFunc, false);
-	};
-	readyListener = aud.addEventListener('canplay', loopObj.readyFunc, false);
-	return loopObj;
+function showAudioWarn() {
+	hideAudioWarn();
+	var warn = document.createElement("span");
+	warn.setAttribute("id", "audioWarnMsg");
+	warn.innerText = "Audio may be blocked. Click page to unblock and hide this message.";
+	warn.style.color = "white";
+	warn.style.fontFamily = "sans-serif";
+	warn.style.padding = "4px 6px";
+	warn.style.background = "rgba(0,0,0,0.5)";
+	warn.style.position = "absolute";
+	warn.style.left = 0;
+	warn.style.top = 0;
+	document.body.appendChild(warn);
+	document.addEventListener('click', hideAudioWarn);
 }
 
-var audioToLoad = 0;
-function audioLoaded() {
-	audioToLoad--;
-	//console.log(audioToLoad+" audio files left to load");
-	if(audioToLoad==0) audioReady();
-}
-
-function audioReady() {
-	console.log("Audio ready");
-	requestAnimationFrame(render);
-	audioLoop.aud.addEventListener('play', ()=>audioLoopReset(audioLoop), false);
-	//audioLoopReset(audioLoop);
-	audioLoop.aud.play().catch((e)=>{
-		var warn = document.createElement("span");
-		warn.setAttribute("id", "audioWarnMsg");
-		warn.innerText = "Audio autoplay was blocked. Click page to enable audio.";
-		warn.style.color = "white";
-		warn.style.fontFamily = "sans-serif";
-		warn.style.padding = "4px 6px";
-		warn.style.background = "rgba(0,0,0,0.5)";
-		warn.style.position = "absolute";
-		warn.style.left = 0;
-		warn.style.top = 0;
-		document.body.appendChild(warn);
-	});
-}
-
-function audioLoopReset(loop) {
-	//console.log("Resetting audio");
-	if(loop.intv) clearInterval(loop.intv);
-	loop.aud.currentTime = 0;
-	var loopOffset = Math.min(0.25, (loop.aud.duration - loop.len) / 2);
-	setTimeout(()=>{		
-		loop.intv = setInterval(()=>{
-			loop.aud.currentTime -= loop.len;// minus buffer length?;
-			//console.log("Looped");
-		}, loop.len*1000);
-	}, loopOffset*1000);
+function hideAudioWarn() {
+	var audioWarnMsg = document.getElementById("audioWarnMsg");
+	if(audioWarnMsg) audioWarnMsg.parentElement.removeChild(audioWarnMsg);
+	document.removeEventListener('click', hideAudioWarn);
 }
